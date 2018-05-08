bf9cf5c4f2753b6e3096bdfe7003e549dbd38258
Raj Sahu <32229344+rjsu26@users.noreply.github.com>
2018-05-08 18:25:33 +0530

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,83 @@
+﻿Param(   [String]$Att, 
+         [String]$Subj,
+         [String]$Body
+     )
+     
+Function Send_EMail 
+{
+    Param( 
+            [Parameter(`
+            Mandatory=$true)]
+            [String]$To , 
+            [Parameter ( `
+            Mandatory=$true)]
+            [String]$From,
+            [Parameter(`
+            Mandatory=$true)]
+            [String]$Password,
+            [Parameter ( `
+            Mandatory=$true)]
+            [String]$subject,
+            [Parameter ( `
+            Mandatory=$true)]
+            [String]$Body,
+            [Parameter ( `
+            Mandatory=$true)]
+            [String]$attachment
+          )
+
+  try
+   {
+    $Msg = New-Object System.Net.Mail.MailMessage($From, $To, $subject, $Body)
+    $Srv = " smtp.gmail.com"
+    if ( $attachment -ne $null)
+    {
+        try 
+        {
+            $Attachments= $attachment -split ( "\:\:");
+
+            ForEach ( $val in $Attachments)
+            {
+                $attch = New-Object System.Net.Mail.Attachment($val)
+                $Msg.Attachments.Add($attch)
+            }
+        }
+
+        catch
+        {
+            exit 2;
+        }
+
+        $client = New-Object net.Mail.SmtpClient($Srv, 587)
+        $client.EnableSsl= $true
+        $client.Credentials = New-Object System.Net.NetworkCredential($From.Split("@")[0], $Password);
+        $client.Send($Msg)
+        Remove-Variable -Name client 
+        Remove -Variable -Name Password
+        exit 7;
+    }
+    }
+
+  catch 
+    {
+        exit 3;
+    }
+
+
+}
+
+try 
+{
+    Send-Email
+        -attachment $Att
+        -To "Address of the recipient"
+        -Body $Body
+        -Subject $Subj
+        -password "your password"
+        -From "Address of the sender"
+}  
+
+catch 
+{
+    exit 4;
+}
